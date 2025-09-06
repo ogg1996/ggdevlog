e066f8ad28f9ffad2101c56dcc523758f180f4b6
ogg1996 <9migoAhri@gmail.com>
2025-09-06 21:22:21 +0900

    #1 뷰어 컴포넌트 제작

---
+++ src/components/viewer.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  category: string;
+  title: string;
+  content: string;
+}
+
+export default function Viewer({ category, title, content }: Props) {
+  return (
+    <div>
+      <h1 className="w-full text-3xl font-bold mb-4 border-none outline-none">
+        {category} {title}
+      </h1>
+      <div className="ql-container ql-snow">
+        <div
+          className="ql-editor"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </div>
+    </div>
+  );
+}
