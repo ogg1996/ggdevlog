a0d169dff3f64855bd9a31524449a2641567cef0
ogg1996 <9migoAhri@gmail.com>
2025-09-11 23:28:14 +0900

    #2 설명 추가(메타 데이터, 게시글 리스트 사용 예정), 썸네일 추가(메타데이터, 게시글 리스트 사용 예정), 작성취소 버튼 추가, 작용 완료 로직 개선(추후 추가 수정 예정)

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,76 +1,45 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @next/next/no-img-element */
 'use client';
 
 import 'react-quill-new/dist/quill.snow.css';
 
-import React, { useMemo, useRef, useState } from 'react';
-import dynamic from 'next/dynamic';
+import React, { useState } from 'react';
 
 import axios from 'axios';
 
 import Viewer from '@/components/viewer';
-
-const ReactQuill = dynamic(
-  async () => {
-    const { default: RQ, Quill } = await import('react-quill-new');
-    const { ImageResize } = await import('quill-image-resize-module-ts');
-
-    Quill.register('modules/ImageResize', ImageResize);
-
-    // eslint-disable-next-line react/display-name
-    return ({ forwardedRef, ...props }: any) => (
-      <RQ ref={forwardedRef} {...props} />
-    );
-  },
-  { ssr: false }
-);
+import Image from 'next/image';
+import QuillEditor from '@/components/quill-editor';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
-  const quillRef = useRef<any>(false);
+  const router = useRouter();
+
+  const [tempImages, setTempImages] = useState<string[]>([]);
 
   const [category, setCategory] = useState('');
   const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [thumbnail, setThumbnail] = useState<{
+    image_name: string;
+    image_url: string;
+  } | null>(null);
   const [content, setContent] = useState('');
 
-  const modules = useMemo(() => {
-    return {
-      toolbar: {
-        container: [
-          [{ header: '1' }, { header: '2' }, { header: '3' }],
-          ['clean', 'bold', 'italic', 'strike', { color: [] }],
-          ['blockquote', 'code-block'],
-          [{ list: 'ordered' }, { list: 'bullet' }],
-          ['image', 'link']
-        ],
-        handlers: {
-          image: handleClickAddImage
-        }
-      },
-      ImageResize: {
-        modules: ['Resize', 'DisplaySize']
-      }
-    };
-  }, []);
-
-  const formats = [
-    'header',
-    'bold',
-    'italic',
-    'strike',
-    'list',
-    'blockquote',
-    'code-block',
-    'link',
-    'image',
-    'color',
-    'background'
-  ];
+  function initializeState() {
+    setTempImages([]);
+    setCategory('');
+    setTitle('');
+    setDescription('');
+    setThumbnail(null);
+    setContent('');
+  }
 
   function handleSelect(e: React.ChangeEvent<HTMLSelectElement>) {
     setCategory(e.target.value);
   }
 
-  function handleClickAddImage() {
+  async function handleClickAddThumbnail() {
     const input = document.createElement('input');
     input.setAttribute('type', 'file');
     input.setAttribute('accept', 'image/*');
@@ -83,16 +52,15 @@ export default function Page() {
         formData.append('img', file);
 
         try {
-          const result = await axios.post(
+          const response = await axios.post(
             'http://localhost:4050/img',
             formData
           );
-          console.log('url: ', result.data.url);
-          const IMG_URL = result.data.url;
 
-          const editor = quillRef.current.getEditor();
-          const range = editor.getSelection();
-          editor.insertEmbed(range.index, 'image', IMG_URL);
+          const IMG_NAME = response.data.img_name;
+          const IMG_URL = response.data.img_url;
+
+          setThumbnail({ image_name: IMG_NAME, image_url: IMG_URL });
         } catch (error) {
           console.error('error: ', error);
         }
@@ -100,46 +68,139 @@ export default function Page() {
     });
   }
 
-  function handleSave() {
-    console.log({ title, content, category });
+  async function handleClickRemoveThumbnail() {
+    try {
+      await axios.delete('http://localhost:4050/img', {
+        data: [thumbnail?.image_name]
+      });
 
+      setThumbnail(null);
+    } catch (error) {
+      console.error('error: ', error);
+    }
+  }
+
+  async function handleSave() {
+    const images: string[] = [];
+
+    for (const image of tempImages) {
+      if (!content.includes(image)) {
+        try {
+          await axios.delete('http://localhost:4050/img', {
+            data: [image]
+          });
+        } catch (error) {
+          console.error('error: ', error);
+        }
+      } else {
+        images.push(image);
+      }
+    }
     // 서버 저장 로직 추가
+    console.log({ title, description, content, category, images, thumbnail });
+
+    initializeState();
+  }
+
+  async function handleCancel() {
+    try {
+      if (thumbnail) {
+        await axios.delete('http://localhost:4050/img', {
+          data: [thumbnail?.image_name]
+        });
+        setThumbnail(null);
+      }
+
+      if (tempImages.length !== 0) {
+        await axios.delete('http://localhost:4050/img', {
+          data: tempImages
+        });
+      }
+      initializeState();
+      router.back();
+    } catch (error) {
+      console.error('error: ', error);
+    }
   }
 
   return (
     <div className="max-w-[700px] mx-auto p-6">
       <div>
-        <div className="mb-4 flex gap-2">
-          <select className="p-2" onChange={handleSelect} value={category}>
-            <option value="">카테고리</option>
-            <option value="Javascript">Javascript</option>
-            <option value="Typescript">Typescript</option>
-            <option value="React">React</option>
-            <option value="Next">Next</option>
-            <option value="TroubleShooting">TroubleShooting</option>
-          </select>
-          <input
-            onChange={e => {
-              setTitle(e.target.value);
-            }}
-            type="text"
-            placeholder="제목"
-            className="w-full pl-2 font-bold border-none outline-none"
-          />
+        <div className="flex gap-2">
+          <div className="grow mb-4 flex flex-col gap-2">
+            <select
+              className="p-2 border border-[rgb(204,204,204)] rounded-[5px]"
+              onChange={handleSelect}
+              value={category}
+            >
+              <option value="">카테고리</option>
+              <option value="Javascript">Javascript</option>
+              <option value="Typescript">Typescript</option>
+              <option value="React">React</option>
+              <option value="Next">Next</option>
+              <option value="TroubleShooting">TroubleShooting</option>
+            </select>
+            <input
+              value={title}
+              onChange={e => {
+                setTitle(e.target.value);
+              }}
+              type="text"
+              placeholder="제목"
+              className="w-full p-2 font-bold border
+              border-[rgb(204,204,204)] rounded-[5px]"
+            />
+            <input
+              value={description}
+              onChange={e => {
+                setDescription(e.target.value);
+              }}
+              type="text"
+              placeholder="간단한 설명"
+              className="w-full p-2 font-bold border
+              border-[rgb(204,204,204)] rounded-[5px]"
+            />
+          </div>
+          {thumbnail === null ? (
+            <button
+              className="flex justify-center items-center w-[141px] h-[141px] 
+              border border-[rgb(204,204,204)] rounded-[5px] cursor-pointer"
+              onClick={handleClickAddThumbnail}
+            >
+              <Image
+                src="/icon-image.png"
+                alt="thumbnail"
+                width={96}
+                height={96}
+              />
+            </button>
+          ) : (
+            <button
+              className="w-[141px] h-[141px] border
+              border-[rgb(204,204,204)] rounded-[5px] cursor-pointer"
+              onClick={handleClickRemoveThumbnail}
+            >
+              <img src={thumbnail.image_url} alt="thumbnail" />
+            </button>
+          )}
         </div>
-        <ReactQuill
-          ref={quillRef}
-          value={content}
-          onChange={setContent}
-          modules={modules}
-          formats={formats}
-          theme="snow"
-          placeholder="내용"
+        <QuillEditor
+          content={content}
+          setContent={setContent}
+          setTempImages={setTempImages}
         />
-        <div className="flex justify-end mt-5">
+        <div className="flex justify-end mt-5 gap-2">
+          <button
+            onClick={handleCancel}
+            className="px-4 py-2 bg-red-400 text-white font-bold 
+            cursor-pointer rounded-lg hover:bg-red-600 transition"
+          >
+            작성취소
+          </button>
           <button
             onClick={handleSave}
-            className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+            className="px-4 py-2 bg-blue-400 text-white font-bold 
+            cursor-pointer rounded-lg hover:bg-blue-600 transition"
           >
             작성완료
           </button>
