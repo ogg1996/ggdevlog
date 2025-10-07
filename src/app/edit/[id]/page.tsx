06ddcd9c621a5aadb6d062297fb1f04f487be763
ogg1996 <9migoAhri@gmail.com>
2025-10-07 16:36:35 +0900

    [#6] API 요청 로직 추가, Editor 컴포넌트 연결

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,9 +1,26 @@
+import Instance from '@/axios/instance';
+import Editor from '@/components/editor';
+
+async function getBoard() {
+  const res = await Instance.get('/board');
+
+  return res.data;
+}
+
+async function getPost(id: string) {
+  const res = await Instance.get(`/post/${id}`);
+
+  return res.data;
+}
+
 export default async function Page({
   params
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+  const board = await getBoard();
+  const post = await getPost(id);
 
-  return <div>{id}</div>;
+  return <Editor boardList={board.data} post={post.data} />;
 }
