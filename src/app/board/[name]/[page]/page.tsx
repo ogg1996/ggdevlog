821a63909f38a8833a8a49c2863fe014bf00de35
ogg1996 <9migoAhri@gmail.com>
2025-11-09 13:32:09 +0900

    [#14] 페이지네이션 기능을 위한 라우트 경로 변경

---
+++ src/app/board/[name]/[page]/page.tsx
@@ -0,0 +1,53 @@
+import Instance from '@/axios/instance';
+import PageNavigationBox from '@/components/page-navigation-box';
+import PostList from '@/components/post-list';
+
+async function getPosts(name: string, page: number = 1, limit: number = 5) {
+  const res = await Instance.get('/post', {
+    params: {
+      board_name: name,
+      page,
+      limit
+    }
+  });
+
+  return res.data;
+}
+
+export default async function Page({
+  params
+}: {
+  params: Promise<{ name: string; page: string }>;
+}) {
+  const CONTENT_LIMIT = 5;
+
+  const { name, page } = await params;
+  const posts = await getPosts(name, Number(page), CONTENT_LIMIT);
+
+  return (
+    <>
+      <div className="h-[200px] bg-[url(/board-thumbnail.png)] bg-cover bg-center mb-7">
+        <div
+          className="w-full h-full bg-[#000000cc] 
+          flex justify-center items-center"
+        >
+          <h2 className="font-[duggeunmo] font-bold text-[32px] text-white">
+            {name}({posts.total})
+          </h2>
+        </div>
+      </div>
+      {posts.data.length !== 0 ? (
+        <>
+          <PostList data={posts.data} />
+          <PageNavigationBox
+            boardName={name}
+            nowPage={posts.page}
+            totalPage={posts.totalPage}
+          />
+        </>
+      ) : (
+        <div>등록된 게시글이 없습니다</div>
+      )}
+    </>
+  );
+}
