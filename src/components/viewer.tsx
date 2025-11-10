fc82f86ebc50274257b989a472bd31f54e0fb916
ogg1996 <9migoAhri@gmail.com>
2025-11-10 17:08:25 +0900

    [#16] edit page css 디자인

--- src/components/viewer.tsx
+++ src/components/viewer.tsx
@@ -1,20 +1,14 @@
 interface Props {
-  title: string;
   content: string;
 }
 
-export default function Viewer({ title, content }: Props) {
+export default function Viewer({ content }: Props) {
   return (
-    <div>
-      <h1 className="w-full text-3xl font-bold mb-4 border-none outline-none">
-        {title}
-      </h1>
-      <div className="ql-container ql-snow">
-        <div
-          className="ql-editor"
-          dangerouslySetInnerHTML={{ __html: content }}
-        />
-      </div>
+    <div className="ql-container ql-snow">
+      <div
+        className="ql-editor"
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
     </div>
   );
 }
