bf5bce194b8a571b3efcaf489d38a5c7ff49fe6d
ogg1996 <9migoAhri@gmail.com>
2025-11-13 00:18:11 +0900

    프록시 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:4050/:path*' // Express 서버 프록시
+      }
+    ];
+  }
 };
 
 export default nextConfig;
