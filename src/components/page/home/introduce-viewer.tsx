f1f9ebfcfeb9885912f0e4453728035cd17fad18
ogg1996 <9migoAhri@gmail.com>
2025-11-19 16:11:58 +0900

    [#18] introduce-viewer 컴포넌트 제작 완료

---
+++ src/components/page/home/introduce-viewer.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import Viewer from '@/components/common/viewer';
+import { useEffect, useRef, useState } from 'react';
+
+interface Props {
+  content: string;
+}
+
+export default function IntroduceViewer({ content }: Props) {
+  const [open, setOpen] = useState(false);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const el = wrapperRef.current;
+
+    if (el) {
+      if (open) {
+        el.style.maxHeight = el.scrollHeight + 'px';
+      } else {
+        el.style.maxHeight = '300px';
+      }
+    }
+  }, [open]);
+
+  return (
+    <>
+      <div
+        ref={wrapperRef}
+        className="relative overflow-y-hidden transition-all duration-200"
+        style={{ maxHeight: '300px' }}
+      >
+        <Viewer content={content} />
+        {!open && (
+          <div
+            className="
+              absolute bottom-0 left-0 right-0 h-30
+              pointer-events-none
+              bg-gradient-to-t from-white to-transparent
+            "
+          />
+        )}
+      </div>
+      {open ? (
+        <button
+          className="w-full h-12 font-bold cursor-pointer
+         text-[#999999] text-[18px] rounded-[5px]
+         hover:text-[#333333] hover:bg-[#eeeeee]"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          접기
+        </button>
+      ) : (
+        <button
+          className="w-full h-12 font-bold cursor-pointer
+         text-[#999999] text-[18px] rounded-[5px]
+         hover:text-[#333333] hover:bg-[#eeeeee]"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          더보기
+        </button>
+      )}
+    </>
+  );
+}
