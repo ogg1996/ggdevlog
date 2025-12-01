c2fb510eca1346788ff9e5b8e79eff99854736bb
ogg1996 <9migoAhri@gmail.com>
2025-12-01 19:30:05 +0900

    [#30] 홈 페이지 메타 데이터 추가 (정적)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
 import Activity from '@/components/page/home/activity';
 import Introduce from '@/components/page/home/introduce';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  metadataBase: new URL('http://localhost:3000'),
+  title: 'GGDevLog - home',
+  description: 'GGDevLog - 나의 개발 기록 저장소',
+  openGraph: {
+    title: 'GGDevLog - home',
+    description: 'GGDevLog - 나의 개발 기록 저장소',
+    images: [
+      {
+        url: '/home-thumbnail.png',
+        alt: '홈 페이지 썸네일'
+      }
+    ],
+    locale: 'ko_KR',
+    type: 'website'
+  }
+};
 
 export default async function Home() {
   return (
