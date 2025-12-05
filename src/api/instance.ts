c73aef070b9f28d4f0fda663bdf1f0064335c797
ogg1996 <9migoAhri@gmail.com>
2025-12-05 19:01:05 +0900

    [#32] instance에 쿠키 포함 설정과 500 미만 오류 해체 설정 추가

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const Instance = axios.create({
-  baseURL: '/api'
+  baseURL: '/api',
+  withCredentials: true,
+  validateStatus: status => status < 500
 });
 
 export default Instance;
