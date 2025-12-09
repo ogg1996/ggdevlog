a841988fc8c867339b39bb8b3d6b583b50166db2
ogg1996 <9migoAhri@gmail.com>
2025-12-10 00:17:44 +0900

    놓친 수정 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Activity from '@/components/page/home/activity';
 import Introduce from '@/components/page/home/introduce';
 
 export const metadata: Metadata = {
-  metadataBase: new URL(`${process.env.NEXT_PUBLIC_META_DATA_BASE_URL}`),
   title: 'GGDevLog - home',
   description: 'GGDevLog - 나의 개발 기록 저장소',
   openGraph: {
@@ -12,7 +11,7 @@ export const metadata: Metadata = {
     description: 'GGDevLog - 나의 개발 기록 저장소',
     images: [
       {
-        url: '/home-thumbnail.webp',
+        url: `${process.env.NEXT_PUBLIC_MY_URL}/home-thumbnail.webp`,
         alt: '홈 페이지 썸네일'
       }
     ],
