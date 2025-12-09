3f58b9893e93c7eee7b17788a7a2299c11778a2e
ogg1996 <9migoAhri@gmail.com>
2025-12-09 12:07:45 +0900

    [#34] 프록시 코드 제거

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:4050/:path*' // Express 서버 프록시
-      }
-    ];
-  }
+  reactStrictMode: false
 };
 
 export default nextConfig;
