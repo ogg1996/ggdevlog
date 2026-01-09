f3640a8892a25d8c923c8d317ae9738464dce093
ogg1996 <9migoAhri@gmail.com>
2026-01-09 20:54:54 +0900

    [#40] 재사용성을 높이기 위해 툴바 버튼 컴포넌트 추가

---
+++ src/components/common/tiptap/toolbar-button.tsx
@@ -0,0 +1,26 @@
+type Props = {
+  title: string;
+  isActive?: boolean;
+  icon: React.ComponentType<{ size?: number; color?: string }>;
+  size: number;
+  onClick: () => void;
+};
+
+export default function ToolbarButton({
+  title,
+  isActive,
+  icon: Icon,
+  size,
+  onClick
+}: Props) {
+  return (
+    <button
+      title={title}
+      className="w-[24px] cursor-pointer  
+      flex justify-center items-center"
+      onClick={onClick}
+    >
+      <Icon size={size} color={isActive ? '#0099FF' : '#999999'} />
+    </button>
+  );
+}
