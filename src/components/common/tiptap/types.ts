03e04b768054321ad2cb66325bad9c8703427d8b
ogg1996 <9migoAhri@gmail.com>
2026-01-09 20:52:57 +0900

    [#40] tiptap 에디터에서 사용되는 타입들 타입 파일로 관리

---
+++ src/components/common/tiptap/types.ts
@@ -0,0 +1,27 @@
+import { Editor } from '@tiptap/react';
+import { LucideIcon } from 'lucide-react';
+
+type ToolbarItem = {
+  key: string;
+  text: string;
+  icon: LucideIcon;
+  action: (editor: Editor) => void;
+};
+
+type EditorState = {
+  heading1: boolean;
+  heading2: boolean;
+  heading3: boolean;
+  bold: boolean;
+  italic: boolean;
+  strike: boolean;
+  underline: boolean;
+  blockquote: boolean;
+  bulletList: boolean;
+  orderedList: boolean;
+  link: boolean;
+};
+
+type EditorKey = keyof EditorState;
+
+export { type ToolbarItem, type EditorState, type EditorKey };
