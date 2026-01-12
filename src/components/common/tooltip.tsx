911af9c9fecda1ee712d3897074e9766c1df32ed
ogg1996 <9migoAhri@gmail.com>
2026-01-13 03:26:11 +0900

    [#40] activity-tooltip을 공용 tooltip으로 변경

---
+++ src/components/common/tooltip.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { useMemo } from 'react';
+
+import Portal from '@/components/common/portal';
+
+interface Props {
+  text: string;
+  targetRect: DOMRect | null;
+  visible: boolean | undefined;
+}
+
+export default function Tooltip({ text, targetRect, visible }: Props) {
+  const pos = useMemo(() => {
+    if (!targetRect) return null;
+
+    let x = targetRect.left + targetRect.width / 2;
+    const y = targetRect.top - 2;
+
+    const estimatedWidth = text.length * 8 + 20;
+
+    const screenWidth = window.innerWidth;
+
+    if (x + estimatedWidth / 2 > screenWidth - 30) {
+      x = screenWidth - estimatedWidth / 2 - 30;
+    }
+
+    if (x - estimatedWidth / 2 < 30) {
+      x = estimatedWidth / 2 + 30;
+    }
+
+    return { x, y };
+  }, [targetRect, text]);
+
+  if (!visible || !pos) return null;
+
+  return (
+    <Portal>
+      <div
+        className="font-[pretendard]
+        fixed px-2 py-1 border border-[#999999]
+        rounded-[5px] bg-white
+        text-[14px] text-[#333333] text-center
+        font-bold whitespace-nowrap"
+        style={{
+          left: pos.x,
+          top: pos.y,
+          transform: 'translateX(-50%) translateY(-100%)'
+        }}
+      >
+        {text}
+      </div>
+    </Portal>
+  );
+}
