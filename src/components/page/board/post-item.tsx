fb3c471723250ae70dc94a97afed875011b653ca
ogg1996 <9migoAhri@gmail.com>
2026-03-12 21:39:27 +0900

    [#67] Board 페이지 게시글 목록 스타일 변경

--- src/components/page/board/post-item.tsx
+++ src/components/page/board/post-item.tsx
@@ -23,13 +23,24 @@ export default function PostItem({
   const created = dayjs(createdAt).tz().format('YYYY. MM. DD');
 
   return (
-    <Link className="group mb-7 flex h-36.25" href={`/post/${id}`}>
-      <div className="min-w-0 grow">
+    <Link
+      className={clsx(
+        'group mb-7 flex rounded-sm',
+        'flex-col-reverse md:flex-row',
+        'bg-gray-100 md:bg-transparent',
+        'dark:bg-slate-800 dark:md:bg-transparent',
+        'shadow-sm shadow-black/20 md:shadow-none dark:shadow-black/80',
+        'transition-all duration-150',
+        'hover:-translate-y-1 hover:shadow-lg md:hover:translate-y-0 md:hover:shadow-none'
+      )}
+      href={`/post/${id}`}
+    >
+      <div className="grow p-3 md:p-0">
         <h2
           className={clsx(
-            'mb-3 h-7.5 truncate',
+            'mb-1 h-7.5 truncate md:mb-3',
             'text-[20px] font-bold',
-            'group-hover:underline',
+            'underline-offset-6 md:group-hover:underline',
             'text-slate-700 dark:text-slate-300',
             'group-hover:text-slate-900 dark:group-hover:text-white'
           )}
@@ -38,7 +49,7 @@ export default function PostItem({
         </h2>
         <p
           className={clsx(
-            'mb-5 h-15.75',
+            'mb-3 h-10.5 md:mb-5 md:h-15.75',
             'overflow-hidden text-[14px] text-ellipsis',
             'text-slate-600 dark:dark:text-slate-400'
           )}
@@ -47,33 +58,36 @@ export default function PostItem({
         </p>
         <p
           className={clsx(
-            'h-4.5 text-[12px]',
+            'h-4.5 text-end text-[12px] md:text-start',
             'text-slate-600 dark:dark:text-slate-400'
           )}
         >
           {created}
         </p>
       </div>
-      <div className="relative ml-10 h-36.25 w-30 shrink-0 overflow-hidden">
+      <div
+        className={clsx(
+          'relative shrink-0 overflow-hidden',
+          'h-50 w-full md:ml-10 md:h-36.25 md:w-70',
+          'rounded-t-sm md:rounded-sm'
+        )}
+      >
         {thumbnailUrl ? (
           <Image
             className="h-full w-full object-cover object-center"
             src={thumbnailUrl}
             alt="썸네일 이미지"
-            sizes="360px"
+            sizes="720px"
             fill
           />
         ) : (
-          <div
-            className={clsx(
-              'h-full w-full font-bold',
-              'flex items-center justify-center',
-              'bg-slate-200/50 dark:bg-slate-800',
-              'text-slate-400 dark:text-slate-400'
-            )}
-          >
-            No Thumbnail
-          </div>
+          <Image
+            className="h-full w-full object-cover object-center"
+            src="/post-thumbnail.webp"
+            alt="썸네일 이미지"
+            sizes="720px"
+            fill
+          />
         )}
       </div>
     </Link>
